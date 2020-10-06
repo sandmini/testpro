75f057202b23880a8002d78cf0e83c108cdd8fb4
hmunuy <iamhmunuy@gmail.com>
2020-10-06 20:06:34 +0700

    3 commit

--- fitm/settings.py
+++ fitm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jxk)q$9**x*d15c6@!mcryg^ihv()*+)ms0i-c%gs*a*&q_mwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition

a8a497324671fdbd49f31795c5dd3ac5142e58bc
hmunuy <iamhmunuy@gmail.com>
2020-10-08 00:13:33 +0700

    k,k

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    uptime = models.IntegerField(default=0)

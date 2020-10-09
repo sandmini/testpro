dd7219c05df16674188e2b64573f02312c35119a
hmunuy <iamhmunuy@gmail.com>
2020-10-09 11:24:05 +0700

    dbdb

--- fitm/settings.py
+++ fitm/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/javascript", ".js", True)
-mimetypes.add_type("text/html", ".html", True)
+# mimetypes.add_type("text/css", ".css", True)
+# mimetypes.add_type("text/javascript", ".js", True)
+# mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,21 +144,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = [
   #  BASE_DIR / "static",
   #  '/var/www/webroot/ROOT/static/',
 #]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = '/ROOT/static/'
+# STATIC_ROOT = '/ROOT/static/'
 
-WHITENOISE_MIMETYPES = {
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.html': 'text/html',
-}
+# WHITENOISE_MIMETYPES = {
+#     '.js': 'text/javascript',
+#     '.css': 'text/css',
+#     '.html': 'text/html',
+# }

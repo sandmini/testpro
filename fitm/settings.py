6451c0aee78268725d5b9a05a869f1defa2df97f
hmunuy <50452855+hmunuy@users.noreply.github.com>
2020-10-09 10:43:12 +0700

    Update settings.py

--- fitm/settings.py
+++ fitm/settings.py
@@ -15,6 +15,8 @@ import os
 import mimetypes
 
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

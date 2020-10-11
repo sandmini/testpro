d16faf381bd9f230aa888ebdad8a280ddfc918b9
hmunuy <iamhmunuy@gmail.com>
2020-10-11 23:36:02 +0700

    ug

--- monitoring/models.py
+++ monitoring/models.py
@@ -7,3 +7,7 @@ class Host(models.Model):
     description = models.CharField(max_length=200)
     uptime = models.IntegerField(default=0)
 
+class test1(models.Model):
+    hostname = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    uptime = models.IntegerField(default=0)

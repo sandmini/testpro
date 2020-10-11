896d60905d4a31a1ca3bfaf92372ce71e0978579
hmunuy <iamhmunuy@gmail.com>
2020-10-12 02:31:57 +0700

    dg

--- fitm/urls.py
+++ fitm/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from monitoring import views
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),

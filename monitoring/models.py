8a043d19cd668cf3636451eae973fcc7b344dfca
hmunuy <50452855+hmunuy@users.noreply.github.com>
2020-10-12 04:53:44 +0700

    Update models.py

--- monitoring/models.py
+++ monitoring/models.py
@@ -6,6 +6,7 @@ class Host(models.Model):
     hostname = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     uptime = models.IntegerField(default=0)
+    insert_time = models.CharField(max_length=200)
 
 class test1(models.Model):
     hostname = models.CharField(max_length=200)
